5f9551d408e6f9254b91ba64d34f093d86e974ca
Jason Gong <jasongong83@gmail.com>
2024-01-27 15:56:54 -0500

    add dev and test gems

--- app/models/therapist.rb
+++ app/models/therapist.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: therapists
+#
+#  id         :uuid             not null, primary key
+#  name       :string           not null
+#  address    :string           not null
+#  city       :string           not null
+#  insurance  :string
+#  remote     :boolean          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Therapist < ApplicationRecord 
   
 end
