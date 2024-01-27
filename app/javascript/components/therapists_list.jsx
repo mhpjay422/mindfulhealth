4bbfd3924506f0c8dc877c90102b4176c7fe5e3a
Jason Gong <jasongong83@gmail.com>
2024-01-27 10:13:17 -0500

    add tailwind

--- app/javascript/components/therapists_list.jsx
+++ app/javascript/components/therapists_list.jsx
@@ -18,26 +18,30 @@ function TherapistsList() {
   if (loading) {
     return <h1>Loading...</h1>;
   } else {
-    return loadedTherapists.map((therapist, index) => (
-      <div key={index}>
-        <table>
+    return (
+      <table>
+        <thead>
           <tr>
-            <th>Name</th>
+            <th className="text-red-500 font-semibold mr-4 ">Name</th>
             <th>Address</th>
             <th>Location</th>
             <th>Insurance</th>
             <th>Remote</th>
           </tr>
-          <tr>
-            <td>{therapist.name}</td>
-            <td>{therapist.address}</td>
-            <td>{therapist.city}</td>
-            <td>{therapist.insurance}</td>
-            <td>{therapist.remote ? "Yes" : "No"}</td>
-          </tr>
-        </table>
-      </div>
-    ));
+        </thead>
+        <tbody>
+          {loadedTherapists.map((therapist, index) => (
+            <tr key={index}>
+              <td className="text-red-500">{therapist.name}</td>
+              <td>{therapist.address}</td>
+              <td>{therapist.city}</td>
+              <td>{therapist.insurance}</td>
+              <td>{therapist.remote ? "Yes" : "No"}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
   }
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./app/views/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+    "./app/assets/stylesheets/**/*.css",
+    "./app/javascript/**/*.js",
+    "./app/javascript/**/*.jsx",
+    // not sure if I need the below here, I'm leaving it for now
+    // "./app/assets/builds/**/*.jsx",
+  ],
+};
