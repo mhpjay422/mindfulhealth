81c26a63c917efa5286f61aeeca51d6ef1bf2183
Jason Gong <jasongong83@gmail.com>
2024-01-29 02:12:45 -0500

    remove comment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,12 @@ Therapist.delete_all
 cities = ["Brooklyn", "Queens", "Bronx", "Manhattan", "Staten Island"]
 insurances = ["Kaiser", "BCBS", "United Health", "Aetna", "Ambetta", "Cigna", "Oscar", "Anthem", "HCSC", "Centene"]
 
-# Using the faker gem, create 10 therapists 
-# 
-
-  30.times do
-    Therapist.create(
-      name: Faker::Name.name,
-      address: Faker::Address.street_address,
-      city: cities[rand(5)],
-      insurance: insurances[rand(10)],
-      remote: Faker::Boolean.boolean(true_ratio: 0.3),
+30.times do
+  Therapist.create(
+    name: Faker::Name.name,
+    address: Faker::Address.street_address,
+    city: cities[rand(5)],
+    insurance: insurances[rand(10)],
+    remote: Faker::Boolean.boolean(true_ratio: 0.3),
   )
-  end
+end
